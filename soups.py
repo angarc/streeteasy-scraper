36e76770fe71fa10fd3d7eb0d55dde7eab08f5f2
Angel Garcia <ag4219@columbia.edu>
2023-03-09 19:12:29 -0500

    Added github actions workflow to build, test, and lint

--- soups.py
+++ soups.py
@@ -6,7 +6,13 @@ def get_page_html(url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) \
 AppleWebKit/537.36 (KHTML, like Gecko) \
-Chrome/108.0.0.0 Safari/537.36"
+Chrome/108.0.0.0 Safari/537.36",
+      'Accept': 'text/html,application/xhtml+xml,\
+application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+      'Accept-Encoding': 'none',
+      'Accept-Language': 'en-US,en;q=0.8',
+      'Connection': 'keep-alive'
     }
     soup = None
 
